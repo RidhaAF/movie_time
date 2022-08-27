d13288764c0d45917dd4995d6b504bd1a0b8fc20
ridhaaf <ridhaaf@gmail.com>
2022-08-27 13:37:36 +0700

    docs: Add env for TMDB API

---
+++ lib/utilities/env_example.dart
@@ -0,0 +1,10 @@
+class Env {
+  static const baseURL = 'YOUR_BASE_URL';
+  static const imageBaseURL = 'YOUR_IMAGE_BASE_URL';
+
+  static const apiKey = 'YOUR_API_KEY';
+  static const apiReadAccessToken = 'YOUR_API_READ_ACCESS_TOKEN';
+
+  static const language = 'en-US';
+  static const region = 'ID';
+}
